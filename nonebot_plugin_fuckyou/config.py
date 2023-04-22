57bff419bc27b8f1e80cc0b8f79969641543fdb3
student_2333 <lgc2333@126.com>
2023-04-22 22:04:20 +0800

    Update config.py

--- nonebot_plugin_fuckyou/config.py
+++ nonebot_plugin_fuckyou/config.py
@@ -9,6 +9,7 @@ class ConfigModel(BaseModel):
     fuckyou_violent: bool = False
     fuckyou_tome: bool = True
     fuckyou_extend_words: Set[str] = set()
+    fuckyou_block: bool = False
 
 
 config: ConfigModel = ConfigModel.parse_obj(get_driver().config.dict())
