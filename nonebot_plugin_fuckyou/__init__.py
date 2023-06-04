8769d0ee10c12012a148a45ea5b7ed77af8c18f3
student_2333 <lgc2333@126.com>
2023-06-05 01:05:35 +0800

    🎉 NoneBot 2.0 🚀

--- nonebot_plugin_fuckyou/__init__.py
+++ nonebot_plugin_fuckyou/__init__.py
@@ -3,11 +3,14 @@ from nonebot.plugin import PluginMetadata
 from .__main__ import get_phase
 from .config import ConfigModel
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __plugin_meta__ = PluginMetadata(
-    "FuckYou",
-    get_phase(),
-    "与机器人对骂即可（关键词触发）",
-    ConfigModel,
-    {"License": "MIT", "Author": "student_2333"},
+    name="FuckYou",
+    description=get_phase(),
+    usage="与机器人对骂即可（关键词触发）",
+    homepage="https://github.com/lgc-NB2Dev/nonebot-plugin-fuckyou",
+    type="application",
+    config=ConfigModel,
+    supported_adapters=None,
+    extra={"License": "MIT", "Author": "student_2333"},
 )
